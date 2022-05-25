48579902a81d9e8ba23e61b02ab9a513c21250ff
Luiz Carlos <luizzlcs@hotmail.com>
2022-05-25 11:22:47 -0300

    first commit

---
+++ bin/exemplo_eudes.dart
@@ -0,0 +1,5 @@
+import 'package:exemplo_eudes/exemplo_eudes.dart' as exemplo_eudes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exemplo_eudes.calculate()}!');
+}
---
+++ test/exemplo_eudes_test.dart
@@ -0,0 +1,8 @@
+import 'package:exemplo_eudes/exemplo_eudes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
